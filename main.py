fd6e115af435a8126c97072b8b03bf1e4019f68a
Ataxanov <asherzod2197@gmail.com>
2026-04-23 20:41:10 +0500

    Add GameCharacter class with attack and heal methods

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+class GameCharacter:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
 
+    def attack(self, damage):
+        self.health -= damage
+        print(f"{self.name} zarar oldi: {self.health} HP qoldi")
+
+    def heal(self, amount):
+        self.health += amount
+        print(f"Tiklandi: {self.health} HP")
+
+
+# Obyekt
+hero = GameCharacter("Ali", 100)
+hero.attack(30)
+hero.heal(20)
